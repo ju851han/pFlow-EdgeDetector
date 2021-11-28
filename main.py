1162a022a9ee03c251796e3a3131b9be7efee663
Julia Hansi <ju851han@htwg-konstanz.de>
2021-11-28 19:51:37 +0100

    add methods for inner polygons

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from polygon import Polygon
 # function definition section
 def save_rectangle():
     """
-    Stores a rectangle
+    Stores nodes (=points, corners) of rectangle
     :return: None
     """
     rectangle = Polygon()
@@ -21,20 +21,34 @@ def save_rectangle():
 
 def save_polygon(point_list, file="polygon.nsv"):
     """
-    Stores a polygon
+    Stores nodes (=points, corners) of a polygon
     :param point_list: list with points which are tuples(x-coordinate, value of y-coordinate)
     :param file: path of file
     :return: None
     """
+    polygon = create_polygon(point_list)
+    polygon.save_to_file(file)
+
+
+def create_polygon(point_list):
+    """
+    Creates nodes (=points, corners) of a polygon
+    :param point_list: list with points which are tuples(x-coordinate, value of y-coordinate)
+    :return: Polygon
+    """
     polygon = Polygon()
     for point in point_list:
         polygon.add_point(point[0], point[1])
-    polygon.save_to_file(file)
+    return polygon
 
 
 # execute section
 # save_rectangle()
-save_polygon([(20, 50), (30, 10), (25, 70)])
-
+rectangle = create_polygon([(100, 100), (100, 500), (800, 500), (800, 100)])
+obstacle1 = create_polygon([(120, 150), (130, 110), (125, 170)])
+obstacle2 = create_polygon([(600, 270), (600, 350), (650, 350), (650, 270)])
+rectangle.add_inner_polygons(obstacle1)
+rectangle.add_inner_polygons(obstacle2)
+rectangle.save_to_file("polygon_with_inner_obstacles.nsv")
 
 # window_width = "1300" , window_height = "610"
--- polygon.py
+++ polygon.py
@@ -8,40 +8,50 @@ class Polygon:
         """
         self.points = []
         self.border_color = 'black'
+        self.inner_polygons = []
+
+    def add_inner_polygons(self, polygon):
+        """
+        Adds an inner-polygon into the list of an outer-polygon.
+        Inner_polygons can be obstacles.
+        :param polygon: Polygon
+        :return: None
+        """
+        self.inner_polygons.append(polygon)
 
     def add_point(self, x, y):
         """
-        Sets a node (=point, corner)
+        Sets a node (=point, corner) and adds it to a list
         :param x: x-coordinate
         :param y: y-coordinate
         :return: None
         """
         self.points.append((x, y))
 
+    @property
     def __transform_pointlist_to_saving_string(self):
         """
-        Converts a pointlist into a string which is later saved in a file
+        Converts a points into a string which is later saved in a file.
         Origin method: pFlowGRID.main.create_save_to_file_string()
         :return: str
         """
-        # save = str(hole_poly_count + 1) + "\n"
+        save = str(len(self.inner_polygons)) + "\n"
 
         # if len(currently_loaded_floor_plan) > 0:
         #   save += currently_loaded_floor_plan + "\n"
         # else:
-        #   save += "-\n"
+        save += "-\n"
 
-        save = "0\n-\n"  # TODO wenn Hindernisse im Grundrisse erstellt werden, diese Zeile löschen und Kommentare einkommentieren
         # count of nodes, then nodes
         save += str(len(self.points)) + "\n"
         for point in self.points:
             save += str(point[0]) + "\t" + str(point[1]) + "\n"
 
-        # holes
-        # for i in range(hole_poly_count + 1):
-        #    save += str(len(hole_polys[i])) + "\n"
-        #   for point in hole_polys[i]:
-        #      save += str(point[0]) + "\t" + str(point[1]) + "\n"
+        # holes or obstacles
+        for polygon in self.inner_polygons:
+            save += str(len(polygon.points)) + "\n"
+            for point in polygon.points:
+                save += str(point[0]) + "\t" + str(point[1]) + "\n"
 
         return save
 
@@ -65,5 +75,5 @@ class Polygon:
         if file is None:
             return
 
-        file.write(self.__transform_pointlist_to_saving_string())
+        file.write(self.__transform_pointlist_to_saving_string)
         file.close()
