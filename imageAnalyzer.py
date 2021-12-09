c1f81523b5c96a7768710040145a5785fb0d15cf
Julia Hansi <ju851han@htwg-konstanz.de>
2021-12-10 00:34:52 +0100

    edit values in mask method

--- imageAnalyzer.py
+++ imageAnalyzer.py
@@ -51,7 +51,8 @@ class ImageAnalyzer:
         :return: mask
         """
         blank = np.zeros(self.image.shape[:2], dtype='uint8')
-        circle = cv2.circle(img=blank, center=(self.image.shape[1] // 2, self.image.shape[0] // 2), radius=100, color=2)
+        circle = cv2.circle(img=blank, center=(self.image.shape[1] // 2, self.image.shape[0] // 2), radius=100,
+                            color=255, thickness=-1)
         # cv2.imshow('Mask', cv2.bitwise_and(src1=self.image, src2=self.image, mask=circle))
         # cv2.waitKey(0)
         return circle
