bfa187eff31ff482cb7e1364ac8485d6df9023cd
ju851han <ju851han@htwg-konstanz.de>
2022-01-17 16:24:47 +0100

    add experiment 4

---
+++ experiments/04_image_sizing.py
@@ -0,0 +1,22 @@
+import imageCleaner
+import os
+
+
+def sizing_images():
+    os.chdir('../training_images')
+    for file_name in os.listdir(os.getcwd()):
+        if file_name.lower().endswith('.png'):
+            image = imageCleaner.transform_image_to_grayscale(file_name)
+            image = imageCleaner.resize_image(image, 1300, 610)
+            # image = imageCleaner.snip_image(image, 0, 1300, 0, 610)
+            image = imageCleaner.rotate_image(image, rotation_degrees=45)
+            imageCleaner.show_image(image, 1, 'Adjusting the Image Size', True)
+
+
+def rotate_floor_plan():
+    floor_plan = imageCleaner.transform_image_to_grayscale("../training_images/floor_plan/005_wry.png")
+    floor_plan = imageCleaner.rotate_image(floor_plan, 43-90)
+    imageCleaner.show_image(floor_plan, 1, 'Rotated Floor Plan', True)
+
+
+rotate_floor_plan()
