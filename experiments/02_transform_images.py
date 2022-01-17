9e456f1afd0af21d0908fc00ee9d7b00693ec974
ju851han <ju851han@htwg-konstanz.de>
2022-01-17 15:49:36 +0100

    edit file type

--- experiments/02_transform_images.py
+++ experiments/02_transform_images.py
@@ -4,6 +4,6 @@ import imageCleaner
 
 os.chdir('../training_images')
 for file_name in os.listdir(os.getcwd()):
-    if file_name.lower().endswith('.png') or file_name.lower().endswith('.jpg'):
+    if file_name.lower().endswith('.png'):
         image = imageCleaner.transform_image_to_grayscale(file_name)
         imageCleaner.show_image(image=image, title=file_name, wait_for_close=True)
