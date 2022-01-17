f6a894d7f5b1c4f3aa1bda96f4191ab754e24b33
ju851han <ju851han@htwg-konstanz.de>
2022-01-17 15:52:55 +0100

    add experiment 3

---
+++ experiments/03_analyze_images.py
@@ -0,0 +1,27 @@
+import imageCleaner
+import imageAnalyzer
+import os
+
+os.chdir('../training_images/')
+
+
+def analyze_images(folder=None):
+    try:
+        if folder is not None:
+            os.chdir(folder)
+    except FileNotFoundError:
+        os.chdir("../" + folder)
+
+    for file_name in os.listdir(os.getcwd()):
+        if file_name.lower().endswith('.png'):
+            image = imageCleaner.load_image(file_name)
+            # image = imageCleaner.transform_image_to_grayscale(file_name)
+            imageAnalyzer.plot_histogram(image, show_hist=False)
+            imageAnalyzer.show_image(image)
+            if file_name == "Dog.png" and folder == "photos":
+                imageAnalyzer.plot_histogram(image, imageAnalyzer.create_round_mask(image=image, center=(500, 700),
+                                                                                    radius=350))  # Dog-face
+
+
+analyze_images("photos")
+analyze_images("floor_plan")
