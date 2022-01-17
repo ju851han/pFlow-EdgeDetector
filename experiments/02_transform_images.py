2593b803961f40f33165a8fccf7036f92deadbd6
ju851han <ju851han@htwg-konstanz.de>
2022-01-17 15:06:12 +0100

    add experiment 2

---
+++ experiments/02_transform_images.py
@@ -0,0 +1,9 @@
+import os
+import imageCleaner
+
+
+os.chdir('../training_images')
+for file_name in os.listdir(os.getcwd()):
+    if file_name.lower().endswith('.png') or file_name.lower().endswith('.jpg'):
+        image = imageCleaner.transform_image_to_grayscale(file_name)
+        imageCleaner.show_image(image=image, title=file_name, wait_for_close=True)
