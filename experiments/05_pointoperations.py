89d38415b61d45f0a4c3339eb70d54b91d87acce
ju851han <ju851han@htwg-konstanz.de>
2022-01-29 21:46:04 +0100

    add docstring

--- experiments/05_pointoperations.py
+++ experiments/05_pointoperations.py
@@ -1,17 +1,34 @@
+""" Experiment 5: Executes different point operations: Thresholding, changing Colors and inverting images-
+
+    Result of apply_threshold():
+    adaptive_threshold-method is for natural images because the structure from the tree and dog (e.g. of the Dog.png) still intact .
+    simple_threshold-method is good for floor_plans.
+    Outlook: Canny Edge Detector uses a Hysterese Threshold which will be described in #TODO Ref
+"""
 import os
 import imageCleaner
 
 os.chdir('../training_images')
 
 
-def apply_threshold(image):
-    simple_thresh = imageCleaner.apply_simple_threshold(image=image, threshold=180)
-    imageCleaner.show_image(image=simple_thresh, title='Changed Color in Area', wait_for_close=True)
-    adaptive_thresh = imageCleaner.apply_adaptive_threshold(image=image, neighborhood_size=51)
-    imageCleaner.show_image(image=adaptive_thresh, title='Changed Color in Area', wait_for_close=True)
+def apply_threshold(gray_image):
+    """ Binaries the image. One time the simple and second time the adaptive threshold will be applied and the result will be displayed.
+
+    :param gray_image: image
+    :return: None
+    """
+    simple_thresh = imageCleaner.apply_simple_threshold(image=gray_image, threshold=180)
+    imageCleaner.show_image(image=simple_thresh, title='Simple Threshold', wait_for_close=True)
+    adaptive_thresh = imageCleaner.apply_adaptive_threshold(image=gray_image, neighborhood_size=51)
+    imageCleaner.show_image(image=adaptive_thresh, title='Adaptive Threshold', wait_for_close=True)
 
 
 def change_color(image):
+    """Marking an area (pixel) of the image.
+
+    :param image: image
+    :return: None
+    """
     colorful_image = imageCleaner.change_color_in_area(image, 130, 170, 370, 430, 0, 128, 255)
     # imageCleaner.change_color_in_pixel(image, 5, 5)
     imageCleaner.show_image(image=colorful_image, title='Changed Color in Area', wait_for_close=True)
@@ -21,7 +38,7 @@ for file_name in os.listdir(os.getcwd()):
     if file_name.lower().endswith('.png'):
         image = file_name
 
-        image = imageCleaner.transform_file_into_grayscale_image(image)
+        image = imageCleaner.transform_file_into_grayscale_image(file_name)
         imageCleaner.show_image(image=image, title='Gray Image', wait_for_close=True)
 
         apply_threshold(image)
