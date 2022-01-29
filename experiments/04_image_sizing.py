da6aaa812c2d4af85bf1e235155e62d8f9d85070
ju851han <ju851han@htwg-konstanz.de>
2022-01-29 20:48:34 +0100

    add resize_image_without_distortion() and docstrings

--- experiments/04_image_sizing.py
+++ experiments/04_image_sizing.py
@@ -1,22 +1,77 @@
-import imageCleaner
+""" Experiment 4: Sizing, snipping and rotating images for displaying in pFlowGRID.
+
+"""
 import os
 
+import imageCleaner
 
-def sizing_images():
-    os.chdir('../training_images')
-    for file_name in os.listdir(os.getcwd()):
-        if file_name.lower().endswith('.png'):
-            image = imageCleaner.transform_file_into_grayscale_image(file_name)
-            image = imageCleaner.resize_image(image, 1300, 610)
-            # image = imageCleaner.snip_image(image, 0, 1300, 0, 610)
-            image = imageCleaner.rotate_image(image, rotation_degrees=45)
-            imageCleaner.show_image(image, 1, 'Adjusting the Image Size', True)
+WINDOW_WIDTH = 1300
+WINDOW_HEIGHT = 610
+
+
+def resize_image(image_path):
+    """ Resizes the image with distortion.
+
+    :return: image
+    """
+    image = imageCleaner.transform_file_into_grayscale_image(image_path)
+    image = imageCleaner.resize_image(image)
+    imageCleaner.show_image(image, 1, 'Adjusting the Image Size', True)
+    return image
+
+
+def resize_image_without_distortion(image_path):
+    """ Resizes the image so that it can be displayed in pFlowGRID.
+
+    :param image_path: str
+    :return: image
+    """
+    image = imageCleaner.transform_file_into_grayscale_image(image_path)
+    if image.shape[1] >= WINDOW_WIDTH:
+        factor = WINDOW_WIDTH / image.shape[1]
+        y_new = int(image.shape[0] * factor)
+        if y_new < WINDOW_HEIGHT:
+            image = imageCleaner.resize_image(image=image, x=WINDOW_WIDTH, y=y_new)
+        else:
+            factor = WINDOW_HEIGHT / image.shape[0]
+            image = imageCleaner.resize_image(image=image, x=int(image.shape[1] * factor), y=WINDOW_HEIGHT)
+    elif image.shape[0] >= WINDOW_HEIGHT:
+        factor = WINDOW_HEIGHT / image.shape[0]
+        image = imageCleaner.resize_image(image=image, x=int(image.shape[1] * factor), y=WINDOW_HEIGHT)
+    return image
+
+
+def snip_image(image_path):
+    """ Snips the image so that it can be displayed in pFlow_Grid.
+
+    :return: image
+    """
+    image = imageCleaner.transform_file_into_grayscale_image(image_path)
+    image = imageCleaner.snip_image(image, 0, 1300, 0, 610)
+    imageCleaner.show_image(image, 1, 'Snipped Image', True)
+    return image
 
 
 def rotate_floor_plan():
-    floor_plan = imageCleaner.transform_file_into_grayscale_image("../training_images/floor_plan/005_wry.png")
-    floor_plan = imageCleaner.rotate_image(floor_plan, 43-90)
+    """ Rotates the image.
+
+    :return: None
+    """
+    try:
+        floor_plan = imageCleaner.transform_file_into_grayscale_image("../training_images/floor_plan/005_wry.png")
+    except FileNotFoundError: 
+        floor_plan = imageCleaner.transform_file_into_grayscale_image("005_wry.png")
+
+    floor_plan = imageCleaner.rotate_image(floor_plan, 43 - 90)
     imageCleaner.show_image(floor_plan, 1, 'Rotated Floor Plan', True)
 
+
+os.chdir('../training_images/floor_plan')
+for file_name in os.listdir(os.getcwd()):
+    if file_name.lower().endswith('.png'):
+        # sized_image = resize_image(file_name)
+        sized_image = resize_image_without_distortion(file_name)
+        imageCleaner.show_image(sized_image, 1, 'sized image', True)
+        # cut_image = snip_image(file_name)
 
 rotate_floor_plan()
