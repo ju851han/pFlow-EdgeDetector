14098960047f76fc8386a843f4549258a9b5d459
ju851han <ju851han@htwg-konstanz.de>
2022-02-19 14:32:33 +0100

    rename titles

--- experiments/e07_canny_combinations.py
+++ experiments/e07_canny_combinations.py
@@ -2,10 +2,10 @@
 
     Result:
     For Pre-processing are following settings recommended:
-    * Gaussian Blur with kernel size=21 (for smoothing the text
-    * Closing with kernel size = 5 and kernel form = Ellipse (for fill the wall and smoothing the text better)
+    * Gaussian Blur with kernel size=21 (for smoothing the text)
+    * Closing with kernel size = 5 and kernel form = Ellipse (for filling the wall and smoothing the text better)
     For Post-Processing is the following setting recommended:
-    * Closing with kernel size = 7 and kernel form = Rectangle (for fill the white corners in the wall)
+    * Closing with kernel size = 7 and kernel form = Rectangle (for filling the white corners in the wall)
 """
 import cv2
 import imageAnalyzer
@@ -28,36 +28,36 @@ titles.append("Grayscale Image")
 image_blur = imageCleaner.add_gaussian_blur(image=image_gray, kernel_size=(21, 21))
 image_canny_orig = imageCleaner.apply_canny_filter(image=image_blur, threshold1=125, threshold2=175)
 images.append(image_canny_orig)
-titles.append("Canny Original")
+titles.append("Gaussian Blur + Canny")
 
 image_epf = imageCleaner.add_edge_preserving_filter(image=image_gray)
 image_canny_epf = imageCleaner.apply_canny_filter(image=image_epf, threshold1=125, threshold2=175)
 images.append(image_canny_epf)
-titles.append("Edge Preserving + Canny")
+titles.append("Edge Preserving Filter + Canny")
 
 image_closing = cv2.morphologyEx(image_blur, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
 image_closed_canny = imageCleaner.apply_canny_filter(image=image_closing, threshold1=125, threshold2=175)
 images.append(image_closed_canny)
-titles.append("Closing + Gauss + Canny")
+titles.append("Closing 5x5 + Gaussian Blur 5x5 + Canny")
 
 image_closed_canny_closing = cv2.morphologyEx(image_closed_canny, cv2.MORPH_CLOSE,
                                               cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
 images.append(image_closed_canny_closing)
-titles.append("Closing + Gauss + Canny + Closing 5x5")
+titles.append("Closing 5x5 + Gaussian Blur 5x5 + Canny + Closing 5x5")
 
 image_closed_canny_closing_biggest = cv2.morphologyEx(image_closed_canny, cv2.MORPH_CLOSE,
                                                       cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (21, 21)))
 images.append(image_closed_canny_closing_biggest)
-titles.append("Closing + Gauss + Canny + Closing Ellipse 21x21")
+titles.append("Closing 5x5 + Gaussian Blur 5x5 + Canny + Closing Ellipse 21x21")
 
 image_closed_canny_closing_big_e = cv2.morphologyEx(image_closed_canny, cv2.MORPH_CLOSE,
                                                     cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7)))
 images.append(image_closed_canny_closing_big_e)
-titles.append("Closing + Gauss + Canny + Closing Ellipse 7x7")
+titles.append("Closing 5x5 + Gaussian Blur 5x5 + Canny + Closing Ellipse 7x7")
 
 image_closed_canny_closing_big = cv2.morphologyEx(image_closed_canny, cv2.MORPH_CLOSE,
                                                   cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)))
 images.append(image_closed_canny_closing_big)
-titles.append("Closing + Gauss + Canny + Closing Rectangle 7x7")
+titles.append("Closing 5x5 + Gaussian Blur 5x5 + Canny + Closing Rectangle 7x7")
 
 imageAnalyzer.show_images(plot_axis=False, number_cols=2, images=images, titles=titles, window_name=file_name)
